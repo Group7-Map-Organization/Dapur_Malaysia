8b6c5717358259e43d457f5abaf70dfa54f9b2c8
atifahzailani <nuratifahipot@gmail.com>
2021-06-03 22:17:50 +0800

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:dapur_malaysia/Screens/Welcome/welcome_screen.dart';
+//import 'package:dapur_malaysia/Screens/Profile/profile_screen.dart';
+import 'package:dapur_malaysia/constants.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dapur Malaysia',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}

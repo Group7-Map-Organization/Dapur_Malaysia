89d57d1db6ddacc99f2358669b87f6ed017dc714
Hasliana Amiza <81730078+haslianamiza@users.noreply.github.com>
2021-06-17 22:01:04 +0800

    Update body.dart

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -1,3 +1,5 @@
+import 'package:dapur_malaysia/Homepage/homepageCreator.dart';
+import 'package:dapur_malaysia/Homepage/homepageHunter.dart';
 import 'package:flutter/material.dart';
 import 'package:dapur_malaysia/Screens/Login/components/background.dart';
 import 'package:dapur_malaysia/Screens/Signup/signup_screen.dart';
@@ -6,11 +8,129 @@ import 'package:dapur_malaysia/components/rounded_button.dart';
 import 'package:dapur_malaysia/components/rounded_input_field.dart';
 import 'package:dapur_malaysia/components/rounded_password_field.dart';
 import 'package:flutter_svg/svg.dart';
+//import 'package:dapur_malaysia/Screens/Homepage/homepageCreator.dart';
+//import 'package:dapur_malaysia/Screens/Homepage/homepageHunter.dart';
+import 'package:dapur_malaysia/User.dart';
+import 'package:dapur_malaysia/Screens/Login/components/Services.dart';
+
+final emailController = TextEditingController();
+final passwordController = TextEditingController();
+
+// ignore: camel_case_types
+class sendEmail {
+  // ignore: non_constant_identifier_names
+  static String emel = '';
+
+  static setEmail(String e) {
+    print(e);
+    emel = e;
+  }
+
+  static getEmail() {
+    return emel;
+  }
+}
+
+// ignore: camel_case_types
+class userData {
+  // ignore: non_constant_identifier_names
+  static List<User> _user;
+
+  static setUser(List<User> u) {
+    print(u);
+    _user = u;
+  }
+
+  static getUser() {
+    return _user;
+  }
+}
 
 class Body extends StatelessWidget {
   const Body({
     Key key,
   }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TransferData(),
+    );
+  }
+}
+
+class TransferData extends StatefulWidget {
+  TransferDataWidget createState() => TransferDataWidget();
+}
+
+class TransferDataWidget extends State {
+  List<User> _user = [];
+
+  // Boolean variable for CircularProgressIndicator.
+  bool visible = false;
+
+  Future webCall() async {
+    // Showing CircularProgressIndicator using State.
+    setState(() {
+      visible = true;
+    });
+
+    // Getting value from Controller
+    String email = emailController.text;
+    String password = passwordController.text;
+
+    sendEmail.setEmail(email);
+
+    Services.getUser(email, password).then((user) {
+      setState(() {
+        _user = user;
+      });
+      print("Length ${user.length}");
+      setState(() {
+        visible = false;
+      });
+      if (user.length == 0) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: new Text('Invalid Login'),
+              actions: <Widget>[
+                FlatButton(
+                  child: new Text("OK"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        var a = _user.map((user) => user.userType);
+        userData.setUser(_user);
+        String userType = a.toString();
+        print(userType);
+        String type1 = '(creator)';
+        String type2 = '(hunter)';
+        if (userType == type1) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return HomepageCreator();
+            },
+          );
+        } else if (userType == type2) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return HomepageHunter();
+            },
+          );
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {

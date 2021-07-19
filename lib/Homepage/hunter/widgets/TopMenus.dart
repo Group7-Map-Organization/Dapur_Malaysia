c62d120ea362d2e2b701b14051777af7dc9fd084
mrymshkri <maryamshukri99@gmail.com>
2021-07-19 21:31:59 +0800

    Create TopMenus.dart

---
+++ lib/Homepage/hunter/widgets/TopMenus.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class TopMenus extends StatefulWidget {
+  @override
+  _TopMenusState createState() => _TopMenusState();
+}
+
+class _TopMenusState extends State<TopMenus> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          TopMenuTiles(name: "Main Dishes", imageUrl: "ic_burger", slug: ""),
+          TopMenuTiles(name: "Desserts", imageUrl: "ic_sushi", slug: ""),
+          TopMenuTiles(name: "Beverages", imageUrl: "ic_soft_drink", slug: ""),
+          TopMenuTiles(name: "Pastry", imageUrl: "ic_cake", slug: ""),
+          TopMenuTiles(name: "Breakfast", imageUrl: "ic_sushi", slug: ""),
+        ],
+      ),
+    );
+  }
+}
+
+class TopMenuTiles extends StatelessWidget {
+  String name;
+  String imageUrl;
+  String slug;
+
+  TopMenuTiles(
+      {Key key,
+      @required this.name,
+      @required this.imageUrl,
+      @required this.slug})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 15, right: 7, top: 7, bottom: 7),
+            decoration: new BoxDecoration(boxShadow: [
+              new BoxShadow(
+                color: Colors.purple[200],
+               blurRadius: 80.0,
+                offset: Offset(0.0, 0.0),
+              ),
+            ]),
+            child: Card(
+                color: Colors.white,
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(3.0),
+                  ),
+                ),
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  child: Center(
+                      child: Image.asset(
+                    'assets/images/topmenu/' + imageUrl + ".png",
+                    width: 24,
+                    height: 24,
+                  )),
+                )),
+          ),
+          Text(name,
+              style: TextStyle(
+                  color: Color(0xFF6e6e71),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400)),
+        ],
+      ),
+    );
+  }
+}

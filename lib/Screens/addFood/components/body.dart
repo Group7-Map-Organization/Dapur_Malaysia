4807bfdf47ab2d8dbd4304b13b98c19c1a410cbc
atifahzailani <nuratifahipot@gmail.com>
2021-07-19 23:53:39 +0800

    Update body.dart

--- lib/Screens/addFood/components/body.dart
+++ lib/Screens/addFood/components/body.dart
@@ -9,7 +9,6 @@ class Body extends StatelessWidget {
   const Body({
     Key key,
   }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;

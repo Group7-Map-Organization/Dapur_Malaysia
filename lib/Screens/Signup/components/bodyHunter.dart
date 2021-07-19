1035a14e7bf63929a5d75deaf97e439a39627124
haslianamiza <haslianamiza@gmail.com>
2021-07-19 13:40:02 +0800

    Update bodyHunter.dart

--- lib/Screens/Signup/components/bodyHunter.dart
+++ lib/Screens/Signup/components/bodyHunter.dart
@@ -34,7 +34,6 @@ class TransferData extends StatefulWidget {
 }
 
 class TransferDataWidget extends State {
-
   final AuthService _auth = AuthService();
   // Boolean variable for CircularProgressIndicator.
   bool visible = false;
@@ -62,7 +61,10 @@ class TransferDataWidget extends State {
       };
 
       // Starting Web Call with data.
-      dynamic response = await http.post(Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC93qNHEHxotzDUhesxY-dDwX2tD8kwTAM'), body: json.encode(data));
+      dynamic response = await http.post(
+          Uri.parse(
+              'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC93qNHEHxotzDUhesxY-dDwX2tD8kwTAM'),
+          body: json.encode(data));
 
       // Getting Server response into variable.
       var message = jsonDecode(response.body);
@@ -158,8 +160,11 @@ class TransferDataWidget extends State {
                 setState(() {
                   visible = true;
                 });
-                dynamic result = await _auth.registerWithEmailAndPassword(nameController.text,emailController.text,passwordController.text);
-                if(result == null){
+                dynamic result = await _auth.registerWithEmailAndPassword(
+                    nameController.text,
+                    emailController.text,
+                    passwordController.text);
+                if (result == null) {
                   setState(() {
                     Text("COULD NOT REGISTER!");
                     visible = false;
@@ -208,37 +213,6 @@ class NameField extends StatelessWidget {
     return TextFieldContainer(
       child: TextField(
         controller: nameController,
-        autocorrect: true,
-        onChanged: onChanged,
-        cursorColor: kPrimaryColor,
-        decoration: InputDecoration(
-          icon: Icon(
-            icon,
-            color: kPrimaryColor,
-          ),
-          hintText: hintText,
-          border: InputBorder.none,
-        ),
-      ),
-    );
-  }
-}
-
-class IcField extends StatelessWidget {
-  final String hintText;
-  final IconData icon;
-  final ValueChanged<String> onChanged;
-  const IcField({
-    Key key,
-    this.hintText,
-    this.icon = Icons.person,
-    this.onChanged,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFieldContainer(
-      child: TextField(
         autocorrect: true,
         onChanged: onChanged,
         cursorColor: kPrimaryColor,
